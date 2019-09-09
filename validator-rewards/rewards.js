0f9a2ecbe001a5bbb04c684d33b140c396d5f523
soc1c <soc1c@users.noreply.github.com>
2019-09-09 11:24:06 +0200

    update validator rewards

--- validator-rewards/rewards.js
+++ validator-rewards/rewards.js
@@ -1,11 +1,13 @@
 const symbol = 'KSM';
 const decimals = 1000000000000;
-const explorer_prefix = "https://polkascan.io/pre/kusama/account/"
+const explorer_prefix = "https://polkascan.io/pre/kusama/account/";
+const date_now = new Date().toGMTString();
 
 const validators = await api.query.staking.validators();
 
+console.log(`Last update: ${date_now}\n\n`);
 console.log("### Validator candidates and rewards:");
 for (let i = 0; i < validators[0].length; i++) {
   const reward = validators[1][i].validatorPayment / decimals;
-  console.log(`- [${validators[0][i]}](${explorer_prefix}${validators[0][i]}): ${reward} KSM`);
+  console.log(`- [\`${validators[0][i]}\`](${explorer_prefix}${validators[0][i]}): ${reward} KSM`);
 }
